2ee84cc16f1581c3a0b52519dd38a4fb17168f44
Hamster3d <hamster3d@gmail.com>
2012-06-11 15:40:56 +0200

    qwe

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ setup(
     license = 'MIT',
     author = 'Alexander Vasilyev',
     author_email = 'hamster3d@gmail.com',
-    packages = ['src'],
-    #packages = find_packages(exclude=['tests']),
+    #packages = ['src'],
+    packages = find_packages(exclude=['tests']),
     classifiers = [
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
