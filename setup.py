80780e15ea289cb2081bab199da3922ceff6741c
Hamster3d <hamster3d@gmail.com>
2013-10-02 22:17:04 +0200

    1.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name = "mathchem",
-    version = "1.0.0",
+    version = "1.0.1",
     description = "",
     long_description = read('README.md'),
     url = 'https://github.com/hamster3d/mathchem-package',
